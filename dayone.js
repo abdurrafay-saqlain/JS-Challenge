4ade0203f89292257ce4ad5de76d78d54cc40b06
abdurrafay-saqlain <abdurrafaysaqlain@hotmail.com>
2024-07-13 18:21:56 +0500

    Day one Completed

---
+++ dayone.js
@@ -0,0 +1,50 @@
+// Activity One
+// var dayOne = 5;
+// console.log(dayOne);
+// Activity One Task 2 
+// let dayOne = "rafay"
+// console.log(dayOne);
+
+//Activity 2
+
+// const activityTwo = true
+// console.log(activityTwo);
+
+// Activity 3
+// String Value
+// let variableOne = 'Rafay'
+// console.log(typeof(variableOne));
+
+// Boolean Value  
+// const variableTwo = false
+// console.log(typeof(variableTwo));
+
+// Activity 4
+// let variable = 5
+// console.log(variable);
+// variable = 9
+// console.log(variable);
+
+// Activity 5
+// const variable = 6
+// variable = 7
+// console.log(variable);
+
+// Feature Request
+// var typeOne = 'Rafay'
+// console.log(typeof(typeOne),typeOne);
+
+// var typeOne = true
+// console.log(typeof(typeOne),typeOne);
+
+// let typeTwo = 23
+// console.log(typeof(typeTwo),typeTwo);
+
+// Reassignment Demo
+// let reassign= 'rafay'
+// reassign = 5
+// console.log(reassign);
+
+// const reassign = 5
+// reassign = 'rafay'
+// console.log(reassign);
