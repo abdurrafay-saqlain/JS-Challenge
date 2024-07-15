cd245a93883673239eed86ad6c059ff87aa114dc
abdurrafay-saqlain <abdurrafaysaqlain@hotmail.com>
2024-07-16 01:01:47 +0500

    control flow structure day three:

---
+++ daythree.js
@@ -0,0 +1,206 @@
+// Activity 1 (Task 1)
+// const prompt = require("prompt-sync")();
+
+const { LEGAL_TLS_SOCKET_OPTIONS, LEGAL_TCP_SOCKET_OPTIONS } = require("mongodb");
+
+// const num = prompt("Enter the number to get a day!. ")
+// const numTwo = prompt("Enter the number 2 ")
+// const numThree = prompt("Enter the number 3 ")
+
+// if (num > 0) {
+//   console.log("Number is positive");
+// }
+
+// else{
+//   console.log("Number is negative");
+// }
+
+// Task 2
+// if (age >=18) {
+//   console.log("You are allowed to vote");
+// }
+// else{
+//   console.log("You are not allowed to vote ! ");
+// }
+
+// Task 3 
+// if(numOne > numTwo && numOne > numThree){
+//   console.log("NumOne is greater");
+// }
+// else if(numTwo > numOne && numTwo > numThree){
+// console.log("Number 2 is greater");
+// }
+// else if(numThree > numOne && numThree > numTwo){
+//   console.log("num three is greater");
+// }
+// else{
+//   console.log("Nothing is greater");
+// }
+
+// Activity 3(Task 4)
+// let day =2;
+// switch (day) {
+//   case 1:
+//     console.log("Monday");
+//      break;
+
+//   case 2:
+//     console.log("Tuestday");
+//     break;
+
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+
+//   case 4:
+//     console.log("Thursday");
+//     break;
+
+//   case 5:
+//     console.log("Friday");
+//     break;
+
+//   case 6:
+//     console.log("Saturday");
+//     break;
+
+//   case 7:
+//     console.log("Sunday");
+//     break;
+
+
+//   default:
+//     console.log("Invalid Number to get a day");
+//     break;
+// }
+// let grade =50
+// switch (grade) {
+//   case 100:
+//     console.log("Grade A ");
+//     break;
+
+//   case 80:
+//     console.log("Grade B ");
+//     break;
+
+//   case 70:
+//     console.log("Grade C ");
+//     break;
+
+//   case 60:
+//     console.log("Grade D ");
+//     break;
+
+//   case 50:
+//     console.log("Grade E ");
+//     break;
+
+//   default:
+//     break;
+// }
+
+// Ternary Operator (Task 6)
+// let num = 9;
+// let result = num % 2 ? 'odd' : 'even'
+// console.log(result);
+
+// Activity 5(Combinign conditons)
+// let num = 58;
+// if (num % 4==0 && num % 100!==0 ) {
+//   console.log("leap year");
+// }
+// else if(num % 400 == 0){
+// console.log("leap year");
+// }
+// else{
+//   console.log("not a leap year!");
+// }
+
+// Feature Request
+
+// let num = -1;
+// if(num > 0){
+//   console.log("positive number");
+// }
+// else{
+//   console.log("negative number! ");
+// }
+
+// Task 2 (Feature Request)
+// let age = 12;
+// if (age >= 18) {
+//   console.log("allowd to vote!");
+// }
+// else{
+//   console.log('not allowed to vote! ');
+// }
+
+// Task three(Feature request)
+
+// let day = 7
+// switch (day) {
+//   case 1:
+//     console.log('Monday');
+//     break;
+//   case 2:
+//     console.log('Tuestday');
+//     break;
+//   case 3:
+//     console.log('Wednesday');
+//     break;
+//   case 4:
+//     console.log('Thurstday');
+//     break;
+//   case 5:
+//     console.log('Friday');
+//     break;
+//   case 6:
+//     console.log('Saturday');
+//     break;
+//   case 7:
+//     console.log('Sunday');
+//     break;
+
+//   default:
+//     break;
+// }
+
+
+// let grade = 4;
+// switch (grade) {
+//   case 1:
+//     console.log('A');
+//     break;
+//   case 2:
+//     console.log('B');
+//     break;
+//   case 3:
+//     console.log('C');
+//     break;
+//   case 4:
+//     console.log('D');
+//     break;
+//   case 5:
+//     console.log('E');
+//     break;
+//   case 6:
+//     console.log('F');
+//     break;
+//   case 7:
+//     console.log('Sunday');
+//     break;
+
+//   default:
+//     break;
+// }
+
+// let leapyear = 90;
+// if (leapyear % 4==0 && leapyear % 100!==0 ) {
+//    console.log("leap year");
+//  }
+//  else if(leapyear % 400 == 0){
+//  console.log("leap year");
+//  }
+//  else{
+//    console.log("not a leap year!");
+//  }
