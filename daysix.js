069087c280702b9dbc97b2ae88a422d8debeb16f
abdurrafay-saqlain <abdurrafaysaqlain@hotmail.com>
2024-07-19 01:40:40 +0500

    array day five done

---
+++ daysix.js
@@ -0,0 +1,132 @@
+// Array Creation task 1 
+
+// const myArray = [1,2,3,4,5]
+// console.log(myArray);
+
+// Task 2
+// const myArray =[1,2,3,4,5]
+// console.log(myArray[0]);
+// console.log(myArray[4]);
+
+// Activity Two task 3
+// const newArray=[1,2,3,4,5]
+// newArray.push(6)
+// console.log(newArray);
+
+// Task 4
+// const newArray=[1,2,3,4,5]
+// newArray.pop()
+// console.log(newArray);
+
+// Task 5
+// const newArray=[1,2,3,4,5]
+// newArray.shift()
+// console.log(newArray);
+
+// Task 6
+
+// const newArray=[1,2,3,4,5]
+// newArray.unshift(6)
+// console.log(newArray);
+
+// Activity 3 
+// const myArray = [1,2,3,4,5]
+// const newArray = myArray.map((num)=>{
+//   return num*2
+// })
+// console.log(newArray);
+// const evenArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 15]
+// const newArray = evenArray.filter((num) => num % 2===0)
+// console.log(newArray);
+// Task 3
+// const myArray=[1,2,3,4,5,6,7,8,9,10]
+// const initialValue=0
+// const newArray = myArray.reduce((acc,curr)=>acc+curr,initialValue)
+// console.log(newArray);
+
+// Array Iteration
+// task 10
+// const myArray =['Rafay',"sunaim",'Shayan']
+// for (let index = 0; index < myArray.length; index++) {
+// }
+// console.log(myArray);
+
+// ForEach Method
+// const myArray=['Rafay','Saqlain','Shayan',"sunaim"]
+// myArray.forEach((name)=> console.log(name))
+
+// Activity 5
+// Multi Dimensionol Array
+
+
+// let MathScore = [
+//   ['John Doe', 20, 60, 'A'],
+//   ['Jane Doe', 10, 52, 'B'],
+//   ['Petr Chess', 5, 24, 'F'],
+//   ['Ling Jess', 28, 43, 'A'],
+//   ['Ben Liard', 16, 51, 'B']
+// ]
+// console.log(MathScore);
+
+// Task 13
+
+// let MathScore = [
+//   ['John Doe', 20, 60, 'A'],
+//   ['Jane Doe', 10, 52, 'B'],
+//   ['Petr Chess', 5, 24, 'F'],
+//   ['Ling Jess', 28, 43, 'A'],
+//   ['Ben Liard', 16, 51, 'B']
+// ]
+// console.log(MathScore[1]);
+
+// Feature Request
+// const myArray = [1,2,3,4,5]
+// myArray.push(6)
+// console.log(myArray);
+
+//  const myArray = [1,2,3,4,5]
+//  myArray.pop()
+//  console.log(myArray);
+
+//  const myArray = [1,2,3,4,5]
+//  myArray.shift()
+//  console.log(myArray);
+
+// const myArray = [1,2,3,4,5]
+// myArray.unshift(3)
+// console.log(myArray);
+
+// const myArray = [1,2,3,4,5]
+// const newArray = myArray.map((item) => item * 2 
+// )
+// console.log(newArray);
+
+// const myArray = [1,2,3,4,5]
+// const newArray = myArray.filter((num) => num > 3 )
+// console.log(newArray);
+
+// const myArray = [1,2,3,4,5]
+// const newArray = myArray.reduce((acc,curr) => acc+curr , 0)
+// console.log(newArray);
+
+// const myArray = [1,2,3,4,5]
+// for (let index = 0; index < myArray.length; index++) {
+//   const element = myArray[index];
+//   console.log(element);
+// }
+
+// const logArrayElements = (element, index) => {
+//   console.log(`a[${index}] = ${element}`);
+// };
+// [2, 5, , 9].forEach(logArrayElements);
+
+// Feature request last
+
+// let MathScore = [
+//   ['John Doe', 20, 60, 'A'],
+//   ['Jane Doe', 10, 52, 'B'],
+//   ['Petr Chess', 5, 24, 'F'],
+//   ['Ling Jess', 28, 43, 'A'],
+//   ['Ben Liard', 16, 51, 'B']
+// ]
+// console.log(MathScore[4][3]);
