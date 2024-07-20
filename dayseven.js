700ae73fa6657db09e7fc29606db51ab09ef3f22
abdurrafay-saqlain <abdurrafaysaqlain@hotmail.com>
2024-07-20 18:37:23 +0500

    day 7 done

---
+++ dayseven.js
@@ -0,0 +1,103 @@
+// Task 1
+// const book = {
+// title:'Book One',
+// author:'AbdurRafay',
+// year:'2020'
+// }
+// console.log(book);
+// Task 2
+
+// const book = {
+// title:'Book One',
+// author:'AbdurRafay',
+// year:'2020'
+// }
+// console.log(book["title","author"]);
+// Activity 2 (Object Methods)
+// const book = {
+// title:'Book One',
+// author:'AbdurRafay',
+// year:'2020',
+// properties:function() {
+//  return (`The title of book is ${this.title} and author is ${this.author}`);
+// }
+// }
+// console.log(book.properties());
+
+// Task 4
+// const book = {
+//   title:'Book One',
+//   author:'AbdurRafay',
+//   year:'2020',
+//   properties:function(year) {
+//    return year = '2024'
+//   }
+//   }
+//   console.log(book.properties());
+
+// Nested Objects
+// Task 5
+// const Books ={
+//   name:{
+//     title:"Rafay",
+//      author:"Rafay Two"
+//   },
+//   book:[]
+// }
+// console.log(Books);
+
+// Task 6
+
+// const Books ={
+//   library:{
+//     title:"Rafay",
+//     name:{
+//       BookName:"Book One"
+//     }
+//   },
+// }
+// console.log(Books.library.name.BookName);
+
+// Activity 4
+// const Books = {
+//   name: "Book One",
+//   title: "Haary Potter",
+//   properties: function () {
+//     return (`The name of Book is ${this.name} and ${this.title}`);
+//   }
+// }
+// console.log(Books.properties());
+
+// Actitivty 5 
+
+// const Books = {
+//   name: "Book One",
+//   title: "Haary Potter",
+//   properties: function () {
+//     return (`The name of Book is ${this.name} and ${this.title}`);
+//   }
+// }
+// console.log(Books.properties());
+
+// Task 8
+// const Books = {
+//   name: "Book One",
+//   title: "Haary Potter",
+//   properties: function () {
+//     return (`The name of Book is ${this.name} and ${this.title}`);
+//   }
+// }
+// for (const num in Books) {
+//   console.log(`The keys are ${num} and the values are ${Books[num]}`);
+//   }
+
+// Task 9
+
+// const Books = {
+//   name: "Book One",
+//   title: "Haary Potter",
+//   properties: function () {
+//     return (`The name of Book is ${this.name} and ${this.title}`);
+//   }
+// }
+// console.log(`The keys of the object are : ${Object.keys(Books) } and the values are : ${Object.values(Books)}`);
